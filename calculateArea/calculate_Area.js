80a7819a66633ee3fea87a8b494820f0bb5c0630
MWardAi <mhward74@gmail.com>
2025-06-10 10:53:42 -0400

    d

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,3 +5,4 @@ function calculateArea(){
     width = parseFloat(document.getElementById("width").value);
     let area = length * width;
      document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
